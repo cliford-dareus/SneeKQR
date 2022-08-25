b54ef7049c697e6778d863c8f8b9bb9c7bf27f23
cliford Dareus <104238549+cliford-dareus@users.noreply.github.com>
2022-08-25 17:34:55 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,62 @@
+const form = document.getElementById('input_form');
+
+
+function onSubmit(e){
+    e.preventDefault();
+
+    const URl = document.getElementById('url_input').value;
+    const size = document.getElementById('size_input').value;
+
+    if(URl === ''){
+        console.log('enter a url')
+    }else{
+        clearHtml();
+        showSpinner();
+
+        setTimeout(() =>{
+            hideSpinner()
+            generateQR(URl,size)
+
+            setTimeout(() => {
+                const saveUrl = document.querySelector('#qr img').src;
+                createSaveUrl(saveUrl);
+            },50)
+        }, 1000)
+    }
+    
+}
+
+function generateQR(url, size){
+    const qrcode = new QRCode(document.getElementById("qr"), {
+        text: url,
+        width: size,
+        height: size,
+    });
+}
+
+function clearHtml(){
+    const html = document.querySelector('#qr')
+    html.innerHTML = '';
+}
+
+function showSpinner(){
+    const spinner = document.querySelector('.spinner');
+    spinner.style.display = 'block'
+}
+
+function hideSpinner(){
+    const spinner = document.querySelector('.spinner');
+    spinner.style.display = 'none'
+}
+
+function createSaveUrl(saveUrl){
+    const link = document.createElement('a');
+    link.id = 'save_link';
+    link.classList = 'btn save_btn';
+    link.href = saveUrl;
+    link.download = 'qrcode';
+    link.innerHTML = 'Save Qr'
+    document.querySelector('.save_btn').appendChild(link);
+}
+
+form.addEventListener('submit', onSubmit);
